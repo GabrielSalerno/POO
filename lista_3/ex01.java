22947e52f0099bf1fc5df69ca6d2a4404478ae2b
Gabriel Salerno <gabriel.salerno.t@gmail.com>
2026-04-01 17:36:30 -0300

    Corrigindo exercício 01 da lista 3

--- lista_3/ex01.java
+++ lista_3/ex01.java
@@ -1,20 +1,12 @@
 package lista_3;
 
-import java.util.Scanner;
-
 public class ex01 {
     public static void main(String[] args){
-        Scanner teclado = new Scanner(System.in);
-
-        int num;
-
-        System.out.print("Digite um número: ");
-        num = teclado.nextInt();
-
-        for(int i=1;i<=10;i++){
-            System.out.printf("%d x %d = %d\n",i,num,i*num);
+        for(int a=1;a<=10;a++){
+            for(int b =1;b<=10;b++){
+                System.out.printf("%d x %d = %d\n",a,b,a*b);
+            }
+            System.out.println("===========");
         }
-
-        teclado.close();
     }
 }
